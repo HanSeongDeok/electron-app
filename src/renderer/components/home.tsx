e9f52d05b9c27e4e12682d75166a2d562de9d850
sdhan <sdhan@suresofttech.com>
2025-06-12 09:36:30 +0900

    [INIT] upstream 초기작업
    - upstream 초기작업 파일 정리

--- src/renderer/components/home.tsx
+++ src/renderer/components/home.tsx
@@ -19,26 +19,14 @@ const Home = memo(() => {
           Learn React
         </a>
         <div className="Flex-row">
-          <NavLink to="/">
+          {/* <NavLink to="/">
             <button className="btn" onClick={sendToMain}>Send Ping</button>
-          </NavLink>
-          <NavLink to="/Other">
-            <button className="btn">Go to Views</button>
-          </NavLink>
-          <NavLink to="/Test">
-            <button className="btn">Go to Test</button>
-          </NavLink>
+          </NavLink> */}
           <NavLink to="/Dock">
             <button className="btn">Go to Dock</button>
           </NavLink>
           <NavLink to="/Mosaic">
             <button className="btn">Go to Mosaic</button>
-          </NavLink>
-          <NavLink to="/Zustand-1">
-            <button className="btn">Go to ZS-1</button>
-          </NavLink>
-          <NavLink to="/Zustand-2">
-            <button className="btn">Go to ZS-2</button>
           </NavLink>
         </div>
       </header>
